b93b9a3c54258df678bd1f6c0116e6d3d12f3e9f
EvgeniyBarabanov <evgeniy.barabanov.96@mail.ru>
2022-05-16 15:02:39 +0300

    continue calculator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
 const calculator = function(){
-    let dataCalc = document.querySelectorAll('[data-calc]');
-    dataCalc = Array.from(dataCalc);
-    dataCalc.forEach(function(item){
-        item.addEventListener('click', function(event){
-            let enter = event.target.dataset.calc;
+    let calcBtn = document.querySelectorAll('.calculator__button');
+    calcBtn.forEach(function(item){
+        item.addEventListener('click', function(){
             let input = document.querySelector('.calculator__conclusion_input');
-            input.value += enter;
-            
-        })
-    })
+            let enter = item.innerHTML;
+
+            const evalFunc = function(){
+                input.value = eval(input.value);
+            };
+
+            const plusAndMinus = function (value) {
+                let test = value.split('');
+                if(test[0] != '-'){
+                    test.unshift('-');
+                    input.value = test.join('');
+                }else{
+                    test.shift();
+                    input.value = test.join('');
+                }
+            }
+
+            if(enter === "+/-"){
+                plusAndMinus(input.value);
+            }else if(enter === "C"){
+                input.value = "";
+            }
+            else if(enter === '='){
+                evalFunc();
+            }else{
+                input.value += enter;
+                console.log(/^-?\d+[+*/-]?[\d+]?$/.test(input.value));
+                
+                
+            }
+        });
+    });
+
 };
+
+
 calculator();
